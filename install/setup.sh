c9f56f77010199a0aaca5341cb2c2fae0b287ebc
Felipe Biaggi Carvalho <fbiaggi@cpqd.com.br>
2023-07-03 14:45:15 -0300

    add docker directory and basic setup script

---
+++ install/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
