3d9bc2399c86709ab06b50aa2634360ee7e54d8d
Felipe Biaggi Carvalho <fbiaggi@cpqd.com.br>
2023-07-03 15:46:38 -0300

    Add neovim plugin

--- install/setup.sh
+++ install/setup.sh
@@ -5,3 +5,5 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
