ae4146869d6fb52ee8c8681d789056feb02b2d48
Felipe Biaggi Carvalho <fbiaggi@cpqd.com.br>
2023-07-03 14:48:36 -0300

    add syntax-highlighting

--- install/setup.sh
+++ install/setup.sh
@@ -3,3 +3,5 @@
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
