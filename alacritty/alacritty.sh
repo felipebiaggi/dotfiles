a930d2f52f10c536faf96ebd0f56ad17d8eb8c00
Felipe Biaggi Carvalho <fbiaggi@cpqd.com.br>
2023-07-04 19:18:26 -0300

    add alacritty install script

---
+++ alacritty/alacritty.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+set -e
+
+apt-get -y install \
+    git \
+    neovim \
+    cmake \
+    pkg-config \
+    libfreetype6-dev \
+    libfontconfig1-dev \
+    libxcb-xfixes0-dev \
+    libxkbcommon-dev \
+    python3 \
+    cargo \
+    curl && \
+    apt autoremove -y
+
+
+cargo install alacritty
+
+cp alacritty.yml ~/.config/alacritty/ 
+
+ # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+#
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#
+# git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
