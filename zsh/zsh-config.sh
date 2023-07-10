c19380e013a27d9f8958dbf49f6139ef3daec253
Felipe Biaggi Carvalho <fbiaggi@cpqd.com.br>
2023-07-10 09:08:12 -0300

    update zsh script

--- zsh/zsh-config.sh
+++ zsh/zsh-config.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+### Set zsh with default shell ###
 chsh -s $(which zsh)
 
+### Copy zsh config ###
 cd -d .zshrc ~/
+
+### Install oh-my-zsh ###
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
