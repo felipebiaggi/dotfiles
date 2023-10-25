d9a607fca04d443069c6e17002d63e766ad8e5b9
akaza <felipebiaggi@gmail.com>
2023-10-25 01:47:28 -0300

    Update zsh-config.sh

--- zsh/zsh-config.sh
+++ zsh/zsh-config.sh
@@ -13,7 +13,6 @@ curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/instal
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
