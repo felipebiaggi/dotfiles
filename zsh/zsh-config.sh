0ba85b6f550bac91b33e8b55918fce5f939a3ec3
akaza <felipebiaggi@gmail.com>
2023-10-25 01:52:39 -0300

    Update zsh-config.sh

--- zsh/zsh-config.sh
+++ zsh/zsh-config.sh
@@ -11,8 +11,13 @@ cd -d .zshrc ~/
 ### Install oh-my-zsh ###
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 
+### Theme ###
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+### Autosuggestions ###
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+### Syntax Highlighting ###
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
